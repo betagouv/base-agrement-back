2a21f2793acbdd8c3d41c067f4329010cdcef806
adrianpaulstaron <adrianpaulstaron@gmail.com>
2026-01-30 17:27:09 +0100

    npx prisma generate in github actions

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(2+2).toBe(4);
     });
   });
 });
