0c0868681d12acdd9d44ae03c6770da15be7268f
adrianpaulstaron <adrianpaulstaron@gmail.com>
2026-01-30 11:28:01 +0100

    wip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    const connectionString = `${process.env.DATABASE_URL}`
+    console.log("connectionString:", connectionString);
+    const adapter = new PrismaPg({ connectionString })
+    const prisma = new PrismaClient({ adapter })
+
+    const hello = await prisma.hello_world.findMany();
+    if (hello.length === 0) {
+      return "tableau vide";
+    }else{    
+      return hello[0].id;
+    }
   }
 }
